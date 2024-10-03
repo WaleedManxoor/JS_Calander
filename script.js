9c9a41b41c01689dc3dcb6f21acdea41fb280bc7
WaleedManxoor <waleed.manxoor8304@gmail.com>
2024-10-03 10:11:06 +0500

    AddedCalculateAgeFunction

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
     var years = currentDate.getFullYear() - dobDate.getFullYear();
     var months = currentDate.getMonth() - dobDate.getMonth();
     var days = currentDate.getDate() - dobDate.getDate();
+
+    function calculateAge() {
+        var dob = document.getElementById('dob').value;
+        
+        if (!dob) {
+            alert('Please enter your date of birth');
+            return;
+        }
