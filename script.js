30e2ed872bec7c2f1d6f44d1bc0fb6d64857eb1f
WaleedManxoor <waleed.manxoor8304@gmail.com>
2024-10-04 11:28:53 +0500

    CompletedJSFile

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-    var dobDate = new Date(dob);
-    var currentDate = new Date();
-
-    var years = currentDate.getFullYear() - dobDate.getFullYear();
-    var months = currentDate.getMonth() - dobDate.getMonth();
-    var days = currentDate.getDate() - dobDate.getDate();
-
     function calculateAge() {
         var dob = document.getElementById('dob').value;
         
@@ -12,3 +5,26 @@
             alert('Please enter your date of birth');
             return;
         }
+
+        var dobDate = new Date(dob);
+        var currentDate = new Date();
+
+        var years = currentDate.getFullYear() - dobDate.getFullYear();
+        var months = currentDate.getMonth() - dobDate.getMonth();
+        var days = currentDate.getDate() - dobDate.getDate();
+    
+        if (days < 0) {
+            months--;  
+            var previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+            days += previousMonth.getDate(); 
+        }
+    
+        if (months < 0) {
+            years--; 
+            months += 12;  
+        }
+        
+        document.getElementById('years').innerHTML = years + " Years";
+        document.getElementById('months').innerHTML = months + " Months";
+        document.getElementById('days').innerHTML = days + " Days";
+}
